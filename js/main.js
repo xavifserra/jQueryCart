fa519a9c9d5bf2a92fe8a4acec3fa584c81de62b
Xavi FS <xavifserra@gmail.com>
2018-05-31 22:45:33 +0200

    pendiente control de errores en el prompt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello!!');
+window.onload = function () {
+
+ $('#todos').on('click', 'li .delete' ,function (e){
+  $(this).parent().remove();
+ });
+
+ $('.btn').click(function(e){
+  var line =(`<li><span>${prompt("New Task:")}</span><div class='delete'>delete</div></li>`);
+  $('ul').append(line);
+ });
+}
